0a349f01896266a74830cd8ce141f526fe9b92a3
Igor Chinchay <igor2008_11@hotmail.com>
2022-01-01 10:09:43 -0500

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.use(helmet());
 //mongodb+srv://admin:locallibrary@cluster0.5cgbr.mongodb.net/local_library?retryWrites=true&w=majority
 const mongoose = require('mongoose');
 const { constant } = require('async');
-const mongoDB = 'mongodb+srv://admin:locallibrary@cluster0.5cgbr.mongodb.net/local_library?retryWrites=true&w=majority';
+const dev_db_url = 'mongodb+srv://admin:locallibrary@cluster0.5cgbr.mongodb.net/local_library?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
