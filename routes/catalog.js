1c6de55ce7fab14b5720ff5cbd6039cd4a8b80aa
Igor Chinchay <igor2008_11@hotmail.com>
2022-01-01 11:44:46 -0500

    Modified routes/catalog.js - bookinstance controller import

--- routes/catalog.js
+++ routes/catalog.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const book_controller = require('../controllers/bookController');
 const author_controller = require('../controllers/authorController');
 const genre_controller = require('../controllers/genreController');
-const book_instance_controller = require('../controllers/bookInstanceController');
+const book_instance_controller = require('../controllers/bookinstanceController');
 
 /// BOOK ROUTES ///
 // GET catalog home page.
